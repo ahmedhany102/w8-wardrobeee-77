54c7e8770aca4f4b354ab8f29b1cbda8f2780727
ahmedhany102 <127224075+ahmedhany102@users.noreply.github.com>
2025-05-18 02:12:11 +0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,7 +20,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
       <AppHeader />
-      <main className="flex-1 w-full pb-32">
+      <main className={`flex-1 w-full ${user && !isAdminPage ? 'pb-32' : ''}`}>
         <div className="container mx-auto px-4 py-4">
           {children}
         </div>
