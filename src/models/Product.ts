3e68b646692f618e774b530e2eda217db2ba9449
ahmedhany102 <127224075+ahmedhany102@users.noreply.github.com>
2025-05-18 03:30:01 +0300

    Update Product.ts

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -15,6 +15,7 @@ export interface Product {
   sizes: ProductSize[];
   colors: string[];
   mainImage: string; // base64 or url
+  images?: string[];
   details?: string;
   hasDiscount?: boolean;
   discount?: number;
