eb4a013ea7c570e859a48f5b89dd53d5375a8eea
أحمد هاني عبد الوهاب <127224075+ahmedhany102@users.noreply.github.com>
2025-12-30 00:47:09 +0200

    Fix formatting issues in VendorsGrid component

--- src/components/VendorsGrid.tsx
+++ src/components/VendorsGrid.tsx
@@ -49,6 +49,8 @@ const VendorsGrid: React.FC = () => {
     );
   }
 
+  
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
       {vendors.map((vendor) => (
